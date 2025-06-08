1a86d0645d11472832823b7626d2bdf97a8408d0
Stephen Schier <stephenjschier@gmail.com>
2025-06-08 11:16:16 -0500

    set up ApolloProvider in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,42 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+// Point to your GraphQL server
+const httpLink = createHttpLink({
+  uri: '/graphql', // Adjust if you're using a proxy or custom domain
+});
+
+// Attach JWT token to requests
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
