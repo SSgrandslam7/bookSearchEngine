48f1bac6d8bdaa1d192bf124ddd7fbc15a3c69c5
Stephen Schier <stephenjschier@gmail.com>
2025-06-09 09:43:03 -0500

    minor changes to server tsconfig.json to make it compatible with render

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,9 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
+const __dirname = path.resolve();
 import apiRoutes from './api/index.js';
 
 router.use('/api', apiRoutes);
