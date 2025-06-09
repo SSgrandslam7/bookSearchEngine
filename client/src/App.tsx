fe0eb9afcca6d709b1b98787f5894651bbd72480
Stephen Schier <stephenjschier@gmail.com>
2025-06-09 12:11:45 -0500

    added vite api url to app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import { setContext } from '@apollo/client/link/context';
 import Navbar from './components/Navbar';
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: import.meta.env.VITE_API_URL + '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
