e46ff3c81a1746179cf5c3066887e2b966cfed5e
Stephen Schier <stephenjschier@gmail.com>
2025-06-09 12:31:29 -0500

    changed line 9 in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/graphql': 'http://localhost:3001',
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'import.meta.env.VITE_API_URL': JSON.stringify(env.VITE_API_URL),
     },
-  },
+  };
 });
